245cc9681b2016f90df74e65c9a76d54d47aa321
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-12-08 20:57:03 +0100

    feat: update of routes and path to components in lazy

--- config/node/frontend/src/routes/Router.js
+++ config/node/frontend/src/routes/Router.js
@@ -1,5 +1,6 @@
 import {createBrowserRouter} from 'react-router-dom';
-import {Home} from './LazyImports';
+import {Home, About, Map, Services, ListOfItems} from './LazyImports';
+
 
 const routes = createBrowserRouter(
     [
@@ -7,9 +8,28 @@ const routes = createBrowserRouter(
             path: '/',
             element: <Home/>
         },
+                {
+            path: '/about',
+            element: <About/>
+        },
+                {
+            path: '/map',
+            element: <Map/>
+        },
+                {
+            path: '/services',
+            element: <Services/>
+        },
+               {
+            path: '/list',
+            element: <ListOfItems/>
+        },
+
+
+
         {
-            path:'*',
-            element:<div>404</div>
+            path: '*',
+            element: <div>404</div>
         }
     ]
 )
