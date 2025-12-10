97c0b382526d78b9c2fcade2b200bd63fe4ebd1c
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-12-10 08:23:21 +0100

    fix: tabulation

--- config/node/frontend/src/routes/Router.js
+++ config/node/frontend/src/routes/Router.js
@@ -1,37 +1,31 @@
 import {createHashRouter} from 'react-router-dom';
 import {Home, About, Map, Services, ListOfItems} from './Lazyimports';
 
-
-const routes = createHashRouter(
-    [
-        {
-            path: '/',
-            element: <Home/>
-        },
-                {
-            path: '/about',
-            element: <About/>
-        },
-                {
-            path: '/map',
-            element: <Map/>
-        },
-                {
-            path: '/services',
-            element: <Services/>
-        },
-               {
-            path: '/list',
-            element: <ListOfItems/>
-        },
-
-
-
-        {
-            path: '*',
-            element: <div>404</div>
-        }
-    ]
-)
+const routes = createHashRouter([
+    {
+        path: '/',
+        element: <Home/>
+    },
+    {
+        path: '/about',
+        element: <About/>
+    },
+    {
+        path: '/map',
+        element: <Map/>
+    },
+    {
+        path: '/services',
+        element: <Services/>
+    },
+    {
+        path: '/list',
+        element: <ListOfItems/>
+    },
+    {
+        path: '*',
+        element: <div>404</div>
+    }
+]);
 
 export default routes;
