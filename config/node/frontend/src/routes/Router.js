9eee166935f51b51b2a7fbdfbd709e81798eea9f
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-12-10 08:18:59 +0100

    fix: spelling mistakes

--- config/node/frontend/src/routes/Router.js
+++ config/node/frontend/src/routes/Router.js
@@ -1,5 +1,5 @@
-import {createHashRouter} from "react-router-dom";
-import {Home, About, Map, Services, ListOfItems} from "./LazyImports";
+import {createHashRouter} from 'react-router-dom';
+import {Home, About, Map, Services, ListOfItems} from './Lazyimports';
 
 
 const routes = createHashRouter(
@@ -8,28 +8,30 @@ const routes = createHashRouter(
             path: '/',
             element: <Home/>
         },
-        {
+                {
             path: '/about',
             element: <About/>
         },
-        {
+                {
             path: '/map',
             element: <Map/>
         },
-        {
+                {
             path: '/services',
             element: <Services/>
         },
-        {
+               {
             path: '/list',
             element: <ListOfItems/>
         },
+
+
+
         {
             path: '*',
             element: <div>404</div>
         }
     ]
 )
-
 
 export default routes;
