b27464f4f3c70dbeab0216d438dac9ce8e910ef9
NataliaBieniek-k <u56113@student.wat.edu.pl>
2026-01-08 03:44:23 +0100

    fix:spelling mistakes

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routers.static_content import router
-from app.routers.db_insert import router_insert
+from routers.static_content import router
+from routers.db_insert import router_insert
 app = FastAPI(title="Mapbook API")
 
 app.include_router(router, prefix="/app")
