b5cb52f2f16d575caefdb51b71ac9c91db532a04
NataliaBieniek-k <u56113@student.wat.edu.pl>
2026-02-05 00:00:42 +0100

    feat: adding button About

--- config/node/frontend/src/pages/Home.jsx
+++ config/node/frontend/src/pages/Home.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import {Typography, Button} from '@mui/material';
+import {Typography, Button, Box} from '@mui/material';
 import {Link} from 'react-router-dom';
+import InfoIcon from '@mui/icons-material/Info';
 
 function Home(props) {
     return (
         <div className='Home'>
             <h1 className='home_title'>GEOPORTAL</h1>
+
             <Typography className='home_subtitle'>
                 Geoportal tematyczny poświęcony danym przestrzennym.
             </Typography>
-            <Button
-                className='home__button'
-                variant='contained'
-                size='large'
-                component={Link}
-                to='services'
-            >
-                START
-            </Button>
+
+            <Box sx={{ display: 'flex', gap: 2, mt: 2, flexDirection: { xs: 'column', sm: 'row' } }}>
+                <Button
+                    className='home__button'
+                    variant='contained'
+                    size='large'
+                    component={Link}
+                    to='services'
+                >
+                    START
+                </Button>
+
+                <Button
+                    variant='outlined'
+                    size='large'
+                    component={Link}
+                    to='about'
+                    startIcon={<InfoIcon />}
+                    sx={{ color: '#e1e1e1', borderColor: '#e1e1e1' }}
+                >
+                    O GEOPORTALU
+                </Button>
+            </Box>
         </div>
     );
 }
+
 export default Home;
